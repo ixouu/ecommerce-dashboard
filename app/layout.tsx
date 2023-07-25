0fe1e42f01bedf75aa00de473ee60e9ee89f3691
ixouu <julienchanon@gmail.com>
2023-07-25 16:08:23 +0200

    created : prisma store table

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <ClerkProvider>
     <html lang="fr_FR">
--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -9,3 +9,5 @@ const prismadb = globalThis.prisma || new PrismaClient();
 // If we init prisma client every time , NextJS with hot reaload will
 // init prisma instances causing warning and degration of performance 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
